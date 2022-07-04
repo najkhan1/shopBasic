06e3358022777eadffa33bb1e1ea0e3bab16dfcc
najkhan01 <naj.khan@sainsburys.co.uk>
2022-07-04 23:50:37 +0100

    Add: functionality to calculate cost from list of items with offers

--- src/main/scala/Basket.scala
+++ src/main/scala/Basket.scala
@@ -1,30 +1,55 @@
 package com.naj
 
-import org.slf4j.LoggerFactory
-
+import com.naj.Items.{apple, orange}
+import com.typesafe.scalalogging._
 import com.typesafe.scalalogging._
 
-case class Basket(items :List[String]) {
 
+
+case class Basket(items :List[String])
+
+case class Products(apples: Int,oranges :Int){
+  def addApple(): Products = this.copy(apples + 1,oranges)
+  def addOrange(): Products = this.copy(apples,oranges + 1)
 }
 object Basket  extends LazyLogging {
-  //val logger: Logger = Logger(LoggerFactory.getLogger(this.getClass))
 
-  def calculateCost(items :List[String]): String = {
-      val cost = items.map(x => itemCost(x.toLowerCase)).foldLeft(0.00)((acc,next) => next match {
-        case Some(value) => acc + value
-        case None => acc
-      })
-      s"£$cost"
+  /*
+  Method to calculate cost of items it takes list of products, and boolean for offer on apples and oranges
+   */
+  def calculateCost(items :List[String],appleOffer :Boolean = false,orangeOffer :Boolean = false): String = {
+    val products = items.foldLeft(Products(0,0))((acc,next) => next match {
+      case value if value.toLowerCase == apple.toString => acc.addApple()
+      case value if value.toLowerCase == orange.toString => acc.addOrange()
+      case otherValue =>
+        logger.warn(s"item: $otherValue it not known")
+        acc
+    })
+   val cost =  (appleOffer,orangeOffer) match {
+      case (true,true) => appleBuyOneGetOneFree(products.apples) + orangeThreeForTwo(products.oranges)
+      case (true,false) => appleBuyOneGetOneFree(products.apples) + (products.oranges * orangePrice)
+      case (false,true) => (products.apples * applePrice) + orangeThreeForTwo(products.oranges)
+      case (false,false) => (products.apples * applePrice) + (products.oranges * orangePrice)
+    }
+    val total = s"£${BigDecimal(cost).setScale(2)}"
+
+    logger.warn(total)
+    total
   }
-  def itemCost(item :String): Option[Double] =  item match {
 
-    case "apple" => Some(0.6)
-    case "orange" => Some(0.25)
-    case value =>
-      logger.warn(s"no price found for item: $value")
-      None
+  def appleBuyOneGetOneFree(numberOfItems :Int): Double = numberOfItems/2 * applePrice + numberOfItems % 2 * applePrice
+  def orangeThreeForTwo(numberOfItems :Int): Double = {
+    val offers = numberOfItems / 3
+    (numberOfItems - offers) * orangePrice
   }
 
+  val applePrice = 0.6
+  val orangePrice = 0.25
+
+}
+
+object Items extends Enumeration{
+  type fruit = Value
+  val apple, orange = Value
 }
 
--- src/test/scala/BasketSpec.scala
+++ src/test/scala/BasketSpec.scala
@@ -7,19 +7,49 @@ class BasketSpec extends AnyFunSpec{
 
   describe("Test Basket"){
     import com.naj.Basket._
-    val methodUnderTest = (itemList :List[String]) =>  calculateCost(itemList)
+    val methodUnderTest = (itemList :List[String],appleOffer :Boolean, orangeOffer :Boolean) =>  calculateCost(itemList,appleOffer, orangeOffer)
 
     it("Should return added cost of all items"){
       val items = List("Apple","Orange","Apple","Apple","Apple")
-      val expected = methodUnderTest(items)
+      val expected = methodUnderTest(items,false,false)
       val actual = "£2.65"
+      actual shouldEqual expected
+
+    }
+    it("Should return added cost of all items with apple offer"){
+      val items = List("Apple","Orange","Apple","Apple","Apple")
+      val expected = methodUnderTest(items,true,false)
+      val actual = "£1.45"
+      actual shouldEqual expected
+
+    }
+    it("Should return added cost of all items with apple offer odd apples"){
+      val items = List("Apple","Orange","Apple","Apple","Apple", "Apple")
+      val expected = methodUnderTest(items,true,false)
+      val actual = "£2.05"
+      actual shouldEqual expected
+
+    }
+
+    it("Should return added cost of all items with apple and orange offer"){
+      val items = List("Apple","Orange","Apple","Apple","Apple", "Apple", "Orange","Orange")
+      val expected = methodUnderTest(items,true,true)
+      val actual = "£2.30"
+      actual shouldEqual expected
+
+    }
+
+    it("Should return added cost of all items with apple and orange offer with oranges not bieng multiple of 3"){
+      val items = List("Apple","Orange","Apple","Apple","Apple", "Apple", "Orange","Orange","Orange")
+      val expected = methodUnderTest(items,true,true)
+      val actual = "£2.55"
       actual shouldEqual expected
 
     }
 
     it("Should return added cost of all items and ignore and log unrecognized items"){
-      val items = List("Apple","Orange","Apple","Apple","Apple","bannana")
-      val expected = methodUnderTest(items)
+      val items = List("Apple","Orange","Apple","Apple","Apple","banana")
+      val expected = methodUnderTest(items,false,false)
       val actual = "£2.65"
       actual shouldEqual expected
 
