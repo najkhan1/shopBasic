0f2a9184a2f2aecf9d3a4b27e21fa1cdb92af7a4
najkhan1 <najkhan184@gmail.com>
2022-10-12 21:52:53 +0100

    Add: functionality to calculate cost from list of items with offers

--- src/main/scala/Basket.scala
+++ src/main/scala/Basket.scala
@@ -1,6 +1,6 @@
 package com.naj
 
-import com.naj.Items.{apple, orange}
+import com.naj.Items.{apple, bannana, orange}
 import com.typesafe.scalalogging._
 import com.typesafe.scalalogging._
 
@@ -8,28 +8,31 @@ import com.typesafe.scalalogging._
 
 case class Basket(items :List[String])
 
-case class Products(apples: Int,oranges :Int){
+case class Products(apples: Int, oranges :Int, bannana: Int){
   def addApple(): Products = this.copy(apples + 1,oranges)
   def addOrange(): Products = this.copy(apples,oranges + 1)
+  def addBannana(): Products = this.copy(apples, oranges, bannana + 1)
 }
 object Basket  extends LazyLogging {
 
   /*
   Method to calculate cost of items it takes list of products, and boolean for offer on apples and oranges
    */
-  def calculateCost(items :List[String],appleOffer :Boolean = false,orangeOffer :Boolean = false): String = {
-    val products = items.foldLeft(Products(0,0))((acc,next) => next match {
+  def calculateCost(items :List[String],appleOffer :Boolean = false,orangeOffer :Boolean = false, bannaOffer :Boolean = false): String = {
+    val products = items.foldLeft(Products(0,0,0))((acc,next) => next match {
       case value if value.toLowerCase == apple.toString => acc.addApple()
       case value if value.toLowerCase == orange.toString => acc.addOrange()
+      case value if value.toLowerCase == bannana.toString => acc.addBannana()
       case otherValue =>
         logger.warn(s"item: $otherValue it not known")
         acc
     })
-   val cost =  (appleOffer,orangeOffer) match {
-      case (true,true) => appleBuyOneGetOneFree(products.apples) + orangeThreeForTwo(products.oranges)
-      case (true,false) => appleBuyOneGetOneFree(products.apples) + (products.oranges * orangePrice)
-      case (false,true) => (products.apples * applePrice) + orangeThreeForTwo(products.oranges)
-      case (false,false) => (products.apples * applePrice) + (products.oranges * orangePrice)
+   val cost =  (appleOffer,orangeOffer,bannaOffer) match {
+      case (true,true, true) => bannanaOffer(appleBuyOneGetOneFree(products.apples),products.bannana) + orangeThreeForTwo(products.oranges)
+      case (true,false,true) => bannanaOffer(appleBuyOneGetOneFree(products.apples),products.bannana) + (products.oranges * orangePrice)
+      case (false,true,true) => bannanaOffer((products.apples * applePrice),products.bannana) + orangeThreeForTwo(products.oranges)
+      case (true,true,false) => bannanaOffer((products.apples * applePrice),products.bannana) + orangeThreeForTwo(products.oranges)
+      case (false,false,false) => (products.apples * applePrice) + (products.oranges * orangePrice) + (0.2 * products.bannana)
     }
     val total = s"£${BigDecimal(cost).setScale(2)}"
 
@@ -43,13 +46,20 @@ object Basket  extends LazyLogging {
     (numberOfItems - offers) * orangePrice
   }
 
+  def bannanaOffer(appleCost :Double, numberOfBannans :Int) :Double = {
+    val bannanaCost = numberOfBannans/2 * bannanaPrice + numberOfBannans % 2 * bannanaPrice
+    if(bannanaCost >= appleCost) bannanaCost else appleCost
+  }
+  
+
   val applePrice = 0.6
   val orangePrice = 0.25
+  val bannanaPrice = 0.2
 
 }
 
 object Items extends Enumeration{
   type fruit = Value
-  val apple, orange = Value
+  val apple, orange, bannana = Value
 }
 
