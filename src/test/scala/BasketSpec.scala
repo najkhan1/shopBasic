fc756cfb91d80f40ab5138ca6c22b2cd2fd2b7b3
najkhan1 <najkhan184@gmail.com>
2022-10-15 01:06:39 +0100

    Add: wip

--- src/test/scala/BasketSpec.scala
+++ src/test/scala/BasketSpec.scala
@@ -3,7 +3,6 @@ package com.naj
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
-
 class BasketSpec extends AnyFunSpec{
 
   describe("Test Basket"){
